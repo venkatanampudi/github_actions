92a47720bbf92eeaa0a78942a4e020fa7d217498
VENKAT ANAMPUDI <venkat.anampudi@gmail.com>
2026-03-02 13:18:39 -0500

    Unit test cases updated

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,9 +1,12 @@
 ### importing from src folder 
 from src.math_operations import add,sub 
 
+
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
+    assert add(-1,2)==1
+    assert add(100,1000 )==1100
 
 def test_sub():
     assert sub(5,3)==2
